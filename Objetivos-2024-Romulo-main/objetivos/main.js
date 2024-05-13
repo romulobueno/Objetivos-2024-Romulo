38eca4b1c3c98457a69c967aba2f3d9dedc36481
romulobueno <160503797+romulobueno@users.noreply.github.com>
2024-05-13 10:15:31 -0300

    Add files via upload

--- Objetivos-2024-Romulo-main/objetivos/main.js
+++ Objetivos-2024-Romulo-main/objetivos/main.js
@@ -11,7 +11,7 @@ const tempoAtual=new Date;
 for(let i=0;i <botoes.length;i++){
     //Variável "i" é igual a 0, mas enquanto essa variável for menor que a quantidade de elementos nessa lista, aumentamos o valor de "i", ou seja, i será a quantidade de botões.
     botoes[i].onclick = function() {
-        for(let j=0;j<botoes.length;j++) {
+        for(let j=0;j<botoes.length;j++);
         botoes[j].classList.remove("ativo");
         textos[j].classList.remove("ativo");
         }
@@ -20,30 +20,31 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo")
         //Adiciona a classe ativo só no botão clicado.
     }
-}
 
+    function calculaTempo(tempoObjetivo){
+        let tempoAtual= new Date();
+        let tempoFinal= tempoObjetivo-tempoAtual;
+        let segundos=Math.floor(tempoFinal/1000);
+        let minutos =Math.floor(segundos/60);
+        let horas=Math.floor(minutos/60);
+        let dias=Math.floor(horas/24);
+    
+        segundos%=60;
+        minutos%=60;
+        horas%=24;
+    
+        return dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+    
+}
+comecaCronometro();
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+function  atualizaCronometro(){
 contadores[0].textContent=calculaTempo(tempoObjetivo1);
-
-function calculaTempo(tempoObjetivo){
-    let tempoAtual= new Date();
-    let tempoFinal= tempoObjetivo-tempoAtual;
-    let segundos=Math.floor(tempoFinal/1000);
-    let minutos =Math.floor(segundos/60);
-    let horas=Math.floor(minutos/60);
-    let dias=Math.floor(horas/24);
-
-    segundos%=60;
-    minutos%=60;
-    horas%=24;
-
-    return dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
-}
-
 contadores[1].textContent=calculaTempo(tempoObjetivo2);
-
-
 contadores[2].textContent=calculaTempo(tempoObjetivo3);
-
-
 contadores[3].textContent=calculaTempo(tempoObjetivo4);
-
+}
